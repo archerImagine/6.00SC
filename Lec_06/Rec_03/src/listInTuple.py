8cd46c4caba7c457e73ae00b1087fb7a1d7a5d4b
archerImagine <archerimagine@gmail.com>
2015-01-06 23:56:51 +0530

    Change for list in a tuple example

--- Lec_06/Rec_03/src/listInTuple.py
+++ Lec_06/Rec_03/src/listInTuple.py
@@ -1,10 +1,23 @@
 tupA = (1,'apple',6.00)
 listA = [0.234,'apple',(1,2),77]
+listB = [0.234,'Banana',(1,2),77]
 tupB = (listA,'MIT',[4,5])
 
 print "tupA: ", tupA
 print "listA: ", listA
 print "tupB: ", tupB
+
+print "\ntupB[0]:", tupB[0]
+print "\ntupB[0][0]:", tupB[0][0]
+
+tupB[0][0] = 0.1
+
+print "\ntupB:", tupB
+print "listA: ", listA
+
+# tupB[0] = listB #gives error
+
+myTemp = tupB[0]
 
 listA.append('Harvard')
 print "After appending to listA: ", listA
