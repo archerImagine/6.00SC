c873f9c7ec7bb8c00483a5ef55db3af1595ce2d1
archerImagine <archerimagine@gmail.com>
2015-01-10 22:25:50 +0530

    Corrected the recursive multiplication

--- Lec_06/Rec_03/src/example08.py
+++ Lec_06/Rec_03/src/example08.py
@@ -6,13 +6,19 @@ def recursiveExponentiation(b,e):
 
 
 print "recursiveExponentiation(3,0): ", recursiveExponentiation(3,0)		
-print "recursiveExponentiation(3,3): ", recursiveExponentiation(3,2)		
+print "recursiveExponentiation(3,2): ", recursiveExponentiation(3,2)		
 
 
 def recursiveMultiplication(m,n):
+	#Base Case
 	if n == 0:
 		return 0
-	elif n == 1:
-		return m
-	elif n == -1:
-		return -m
+	elif n >= 1 :
+		return m + recursiveMultiplication(m,n-1)
+	elif n <= -1 :
+		return -m + recursiveMultiplication(m,n+1)
+
+
+
+print "recursiveMultiplication(3,3): ", recursiveMultiplication(3,3)		
+print "recursiveMultiplication(3,2): ", recursiveMultiplication(3,2)		
--- Lec_07/Rec_04/ReadMe.md
+++ Lec_07/Rec_04/ReadMe.md
@@ -1,1 +1,7 @@
+# MIT 6.00SC | Recitation 04 | Recursion, Pseudo code and Debugging #
+
+## [Recursion ](https://www.youtube.com/watch?v=7BpomdjZ_Os&feature=player_detailpage&list=PLB2BE3D6CA77BB8F7#t=32) ##
+
+We will start from the last recitation's topic, Recursion.
+
 
