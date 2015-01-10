3a792acc94baed4ace273e3fbd8d1888e452e546
archerImagine <archerimagine@gmail.com>
2015-01-10 23:01:19 +0530

    recursion vs iteration example

--- Lec_07/Rec_04/src/example02.py
+++ Lec_07/Rec_04/src/example02.py
@@ -10,13 +10,14 @@ def iteratativeFibonacci(n):
 		# Hold the currect and the previous Fibonacci number
 		previousFib = 0
 		currentFib = 1
-		for iteration in xrange(1,n):
+		for iteration in xrange(1,n + 1): 	#MIT OCW, has a bug, it should not be xrange(1,n)
 			# The next Fib number will be sum of currentFib and previousFib
 			nextFib = previousFib + currentFib
 			# print "nextFib: ", nextFib
 			# Save these value
 			previousFib = currentFib
 			currentFib = nextFib
+			# print "iteration: ", iteration, " currentFib : ", currentFib, " previousFib: ", previousFib, "\n"
 	return currentFib
 
 
@@ -25,3 +26,7 @@ def testFib(n):
 		print ('fib of', i, '=', iteratativeFibonacci(i))
 
 testFib(5)
+
+#print "iteratativeFibonacci(0) : ", iteratativeFibonacci(0)
+#print "iteratativeFibonacci(1) : ", iteratativeFibonacci(1)
+# print "iteratativeFibonacci(2) : ", iteratativeFibonacci(2)
