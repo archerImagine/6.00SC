e747a1ceea3f338c76f6fc887e8d35421b321679
archerImagine <archerimagine@gmail.com>
2015-01-14 22:05:33 +0530

    euclid GCD function.

---
+++ Misc/src/euclid.py
@@ -0,0 +1,18 @@
+def euclid(a,b):
+	if b == 0:
+		return a
+	else:
+		return euclid(b,a%b)
+
+print euclid(21,9)
+
+def extendedEuclid(a,b):
+	if b == 0:
+		return (a,1,0)
+	else:
+		(dNew,xNew,yNew) = extendedEuclid(b, a%b)
+		(d,x,y) = (dNew,yNew,(xNew - ((a/b) * yNew)))
+		print (d,x,y)
+		return (d,x,y)
+
+print extendedEuclid(99,78)		
